f9b0b88618817b64c1d84f2cf6b51d47cb70fa87
Daniel Quintela Muniz <danielquintelamuniz@unisantos.br>
2024-08-28 11:06:54 -0300

    alterações feitas na aula

--- src/frota.py
+++ src/frota.py
@@ -2,28 +2,41 @@ class Carro:
     modelo : str
     marca : str
     cor : str
-    odometro : 0.0
-    motor_on : False
+    __odometro : 0.0
+    __motor_on : False
+    __tanque = 0.0
+    consumo_medio : float
 
     def __init__(self, modelo : str, marca : str, cor : str,
-                       odometro : float, motor : bool):
+                       odometro : float, motor : bool, tanque : float, consumo_medio : float):
         self.modelo = modelo
         self.marca = marca
         self.cor = cor
-        self.odometro = odometro
+        self.__odometro = odometro
         self.motor_on = motor
+        self.__tanque = tanque
+        self.consumo_medio = consumo_medio
 
     def ligar(self):
-        if not self.motor_on:
+        if not self.motor_on and self.__tanque > 0:
             self.motor_on = True
         else:
-            raise Exception("Erro: Motor já ligado!")
+            raise Exception("Erro: Motor já ligado ou tanque vazio!")
 
     def acelerar(self, velocidade : float, tempo : float):
-        if self.motor_on:
-            self.odometro += velocidade * tempo
+        if self.motor_on and self.__tanque > 0:
+            km = velocidade * tempo
+            litros = km / self.consumo_medio
+
+            if self.__tanque >= litros:
+                self.__tanque -= litros
+            else:
+                km = litros * self.consumo_medio
+                self.__tanque = 0
+                self.desligar()
+                self.__odometro += km
         else:
-            raise Exception("Erro: Não é possível acelerar! Motor desligado!")
+            raise Exception("Erro: Não é possível acelerar! Motor desligado ou sem combustível!")
 
     def desligar(self):
         if self.motor_on:
@@ -33,9 +46,15 @@ class Carro:
 
     def __str__(self):
         info = (f'Carro {self.modelo}, marca {self.marca}, '
-                f'cor {self.cor}\n{self.odometro} Km, '
+                f'cor {self.cor}\n{self.__odometro} Km, '
                 f'motor {self.motor_on}')
         return info
+    
+    def get_odometro(self):
+        return self.__odometro
+
+    def get_tanque(self):
+        return self.__tanque
 
 
 
--- src/main.py
+++ src/main.py
@@ -1,39 +1,54 @@
 from frota import *
+def operar_carro(carro : Carro):
+    print('1- Ligar motor')
+    print('2- Desligar motor')
+    print('3- Acelerar')
+
+    op = 0
+    while op not in (1, 2, 3):
+        op = int(input("Digite as opcoes[1-3]: "))
+
+        if op == 1:
+            carro.ligar()
+        elif op == 2:
+            carro.desligar()
+        elif op == 3:
+            v = float(input("Informe a velocidade: "))
+            t = float(input("Informe o tempo: "))
+            carro.acelerar(v, t)
+
+            print('Infos atuais do carro')
+            print(carro)
 
 if __name__ == "__main__":
-    print('Cadastre um carro')
+    print('Cadastre o 1o carro')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input('Nivel do tanque: '))
+    cm = float(input('Consumo medio: '))
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, cm)
 
-    kms = float(input('Digite com quantos Kms: '))
-
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, motor = True)
+    print('Cadastre o 2o carro')
+    nm_modelo = input('Digite o modelo: ')
+    nm_marca = input('Digite a marca: ')
+    nm_cor = input('Digite a cor: ')
+    litros = float(input('Nivel do tanque: '))
+    cm = float(input('Consumo medio: '))
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, cm)
 
     '''
-    Controlando o carro até ele atingir 10000 Km
+    Controlando 2 carros até ele atingir 10000 Km
     '''
-    while carro1.odometro < 10000:
+    while carro1.odometro < 600 and carro2.odometro < 600 and (carro1.tanque > 0 or carro2.tanque > 0):
         try:
-            print('1- Ligar motor')
-            print('2- Desligar motor')
-            print('3- Acelerar')
-
             op = 0
-            while op not in (1,2,3):
-                op = int(input("Digite as opcoes[1-3]: "))
-
+            while op not in (1,2):
+                op = int(input("Qual carro quer operar? [1-2]: "))
             if op == 1:
-                carro1.ligar()
-            elif op == 2:
-                carro1.desligar()
-            elif op == 3:
-                v = float(input("Informe a velocidade: "))
-                t = float(input("Informe o tempo: "))
-                carro1.acelerar(v, t)
-
-            print('Infos atuais do carro')
-            print(carro1)
+                operar_carro(carro1)
+            else:
+                operar_carro(carro2)
         except Exception as e:
             print("Erro!")
             print(e)
